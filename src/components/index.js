22fb2a257ed517adec64848911d1451d5db7633e
Marian-Janolek <m.janolek@gmail.com>
2021-11-18 17:27:51 +0100

    add layout and routes

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Navbar from './Navbar';
+import News from './News';
+import Cryptocurrencies from './Cryptocurrencies';
+import CryptoDetails from './CryptoDetails';
+import HomePage from './HomePage';
+import Exchanges from './Exchanges';
+
+export { Navbar, News, Cryptocurrencies, CryptoDetails, HomePage, Exchanges };
